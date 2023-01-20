ad68c9334632d43e142da42a3cf8d93c1cbe6a02
err415 <117043822+err415@users.noreply.github.com>
2023-01-20 17:14:55 +0300

    Delete src directory

--- src/pages/welcome/components/index.js
+++
@@ -1,9 +0,0 @@
-import Handlebars from 'handlebars';
-import tpl from 'bundle-text:./tpl.hbs';
-import './style.css';
-
-Handlebars.registerPartial('h1Welcome', tpl);
-
-export default (id, value, spanValue) => {
-    return Handlebars.compile(tpl)({ id, value, spanValue });
-}
