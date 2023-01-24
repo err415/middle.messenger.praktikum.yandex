84187c8c72275bc16f3ebe2e7b8a6ad267e3ac43
err415 <uch.akk@yandex.ru>
2023-01-24 14:04:00 +0300

    Edit Stage, Profile Template.

--- src/state.js
+++ src/state.js
@@ -7,16 +7,16 @@ export const state = {
     h1: 'Title',
     content: 'Содержимое',
     fname: 'students',
-    welcomeButton: button('welcome-btn','Войти'),
+    welcomeButton: button('welcome-btn', 'Войти'),
 
     signin: {
         title: 'Авторизация',
-        signinButton: button('welcome-btn','ДАЛЕЕ'),
-        signinPassButton: button('welcome-btn','ВХОД'),
+        signinButton: button('welcome-btn', 'ДАЛЕЕ'),
+        signinPassButton: button('welcome-btn', 'ВХОД'),
     },
     signup: {
-      title: 'Регистрация',
-        signupButton: button('welcome-btn','Зарегистрироваться'),
+        title: 'Регистрация',
+        signupButton: button('welcome-btn', 'Зарегистрироваться'),
 
     },
     main: {
@@ -27,18 +27,41 @@ export const state = {
         edit: {
             title: 'Изменить профиль',
             login: 'Mark',
-            mainFormLabel: 'Имя (обязательно)',
-            slaveFormLabel: 'Имя пользователя',
-            buttonPasswordChange: 'Сменить пароль',
-            mainFormLastNamePlaceholderOne: '"Фамилия (необязательно)"',
-            mainFormLastNamePlaceholderTwo: '"О себе"',
-            descriptonFormMain: 'Любые подробности, например: возраст, род занятий или город. \n Пример: 20 лет, сам себе программист из МСК.',
-            discriptionFormSlaveOne: 'Вы можете выбрать публичное имя пользователя в Spectre. В\n' +
-                'этом случае - другие люди смогут найти вас по такому имени и\n' +
-                'связаться, не зная вашего телефона.',
-            discriptionFormSlaveTwo: 'Вы можете использовать символы a-z, 0-9 и подчеркивания.\n' +
-                '            Минимальная длина - 5 символов.',
-        }
+            button: {
+                pwdChange: 'Сменить пароль',
+            },
+            mainForm: {
+                data:
+                    {
+                        last_name: '"Фамилия (необязательно)"',
+                        first_name: '',
+                        about_myself: '"О себе"',
+                        phone: '"8 (999) 999 99-99"',
+                        email: 'testtest@yandex.ru',
+                        nickname: '"Фаина Раневская"',
+                    },
+                label: {
+                        first_name: 'Имя (обязательно)',
+                },
+                description: {
+                    d1: 'Любые подробности, например: возраст, род занятий или город. \n Пример: 20 лет, сам себе программист из МСК.',
+                }
+            },
+            slaveForm: {
+                data: {
 
+                },
+                    label: {
+                        login: 'Имя пользователя',
+                },
+                description: {
+                    d1: 'Вы можете выбрать публичное имя пользователя в Spectre. В\n' +
+                        'этом случае - другие люди смогут найти вас по такому имени и\n' +
+                        'связаться, не зная вашего телефона.',
+                    d2: 'Вы можете использовать символы a-z, 0-9 и подчеркивания.\n' +
+                        'Минимальная длина - 5 символов.',
+                },
+            },
+        }
     }
 }
